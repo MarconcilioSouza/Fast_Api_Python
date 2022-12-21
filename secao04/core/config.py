418e2e76a6ed1babeeb0bf8d030e02f91a0c0d64
Marconcilio Souza <marconcilioka@kalunga.com.br>
2022-12-21 16:36:32 -0300

    main

--- secao04/core/config.py
+++ secao04/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     Configurações gerais usadas na aplicação
     """
     
-    API_V1_STG: str = '/api/v1'
+    API_V1_STR: str = '/api/v1'
     DB_URL: str = "postgresql+asyncpg://postgres:123@localhost:5432/Faculdade"
     DBBaseModel = declarative_base()
     
